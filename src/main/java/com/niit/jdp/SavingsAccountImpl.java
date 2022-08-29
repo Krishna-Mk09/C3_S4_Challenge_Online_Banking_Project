9669173bfde83ae1d356591856bd78ee561f05e1
M V K <krishna.mugala9@gmail.com>
2022-08-29 12:39:48 +0530

    initialised the objects

--- src/main/java/com/niit/jdp/SavingsAccountImpl.java
+++ src/main/java/com/niit/jdp/SavingsAccountImpl.java
@@ -10,6 +10,11 @@ package com.niit.jdp;
 
 public class SavingsAccountImpl {
     public static void main(String[] args) {
-
+        Bank bank = new Bank("indian OverSeas Bank", "karimnagar", "253/633/hyderabad", "ioba332255");
+        SavingsAccount savingsAccount = new SavingsAccount("overseas", "hyderabad", "6347/3434/karimnagar", "ioba3322"
+                , 131234345
+                , "vamshi"
+                , 5000.0
+                , "active");
     }
 }
