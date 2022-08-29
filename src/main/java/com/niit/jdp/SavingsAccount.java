2abf91d5e83f3540eecbf7b62f1671efbe7578ec
M V K <krishna.mugala9@gmail.com>
2022-08-29 12:12:10 +0530

    added fields to the savings account class

--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -9,4 +9,8 @@
 package com.niit.jdp;
 
 public class SavingsAccount {
+    private long accountNumber;
+    private String accountHoldersName;
+    private double accountBalance;
+    private String accountStatus;
 }
